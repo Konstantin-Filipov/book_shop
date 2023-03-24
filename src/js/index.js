ec737ae6c12e14c1c2f3cb804837e22ea602f9b7
Konstantin-Filipov <kosio1234545@gmail.com>
2023-03-24 17:43:52 +0100

    generate html doc for each book element event handler for 'purchase' hyperlink addToCart function

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+async function addToCart(selectedTitle, selectedPrice) {
+  console.log(`${selectedTitle.textContent} has been added to cart`);
+  console.log(` for ${selectedPrice.textContent}`);
+}
 let booksState = [];
 
 /** Get json data by url, return data */
@@ -31,23 +35,64 @@ function loadHome(bookState) {
     <div class="col-xxl-2 col-xl-3 col-lg-3 col-md-4 col-sm-6">
      
     <div class="card card-img-top h-100">
-        <img src="https://s1.adlibris.com/images/28652140/designing-user-experience.jpg" />
+        <img src="${book.img}" />
         <div class="card-body">
             <h5 class="card-title"> Title: ${book.title} </h5>
-            <h6 class="card-secondary"> Author: ${book.author} </h6>
-            <h6 class="card-secondary"> Price: ${book.price} kr </h6>
-            <h6 class="card-secondary"> Category: ${book.category} kr </h6>
+            <h6 class="card-author"> Author: ${book.author} </h6>
+            <h6 class="card-price"> Price: ${book.price} kr </h6>
+            <h6 class="card-category"> Category: ${book.category} </h6>
         </div>
         <div class="card-body">
-            <a href="#" class="card-link">Details</a>
-            <a href="#" class="card-link">Purchase</a>
+            <a href="#" id="popDetail" class="card-link" data-bs-toggle="modal" data-bs-target="#popModal">Details</a>
+            <a href="#" id="purchase" class="card-link">Purchase</a>
         </div>
     </div>
+    </div>
+
+    <div class="modal fade" id="popModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Book title: ${book.title} </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h6 class="card-secondary"> Author: ${book.author} </h6>
+                        <h6 class="card-secondary"> Price: ${book.price} kr </h6>
+                        <h6 class="card-secondary"> Category: ${book.category} </h6>
+                        <h6 class="card-secondary"> Description: ${book.category} </h6>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Purchase</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
     </div>
     `;
   });
 
   booksRow.innerHTML = booksHtml;
+  let purchases = document.querySelectorAll("#purchase");
+
+  /** Add event listen to purchase element, when it is clicked,
+   * add the book.
+   */
+  purchases.forEach((purchase) => {
+    purchase.addEventListener("click", (event) => {
+      event.preventDefault();
+      let selectedTitle =
+        purchase.parentElement.parentElement.querySelector(".card-title");
+      let selectedPrice =
+        purchase.parentElement.parentElement.querySelector(".card-price");
+      addToCart(selectedTitle, selectedPrice);
+    });
+  });
+  console.log(purchase);
 }
 
 /** entry point to index.html
@@ -57,8 +102,8 @@ function loadHome(bookState) {
 function loadMain() {
   console.log("come to load main");
   document.querySelector("main").innerHTML = `
-    <div class="container rounded m-2">
-      <div id="books" class="row text-center g-3 flex-row justify-content-center">
+    <div class="container rounded m2-auto ">
+      <div id="books" class="row text-center mt-2 g-3 flex-row justify-content-center">
       <h1>hahaha</h1>
       </div>
     </div>`;
